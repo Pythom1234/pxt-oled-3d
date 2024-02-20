6db8fe99fba2b04a9632bb4ddf21f56dd4713823
Pythom1234 <pythom1234@email.cz>
2024-02-20 15:28:30 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 //% block="OLED 3D" icon="\uf1b2" color=#0000ff
 namespace OLED_3D {
-    //% block
-    export function a() {}
+    export class Cube {
+        constructor() {}
+    }
+    let objects = []
+    //% block="initalize OLED display"
+    //% block.loc.cs="inicializovat OLED display"
+    export function init() {
+        OLED.init()
+    }
 }
