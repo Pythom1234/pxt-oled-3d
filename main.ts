80e2cbbc36ed0e28b43b9faf825f68a7af7bf7c5
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-02-21 15:56:31 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,65 @@
+
+//% block="OLED 3D" icon="\uf1b2" color=#0000ff
+namespace OLED_3D {
+    let objects: any[] = []
+    let cameraPos = [0, 0, 0]
+    let cameraRotation = [0, 90, 0]
+    export class Cube {
+        public x: number
+        public y: number
+        public z: number
+        public size: number
+        constructor(_x: number, _y: number, _z: number, _size: number) {
+            this.x = _x
+            this.y = _y
+            this.z = _z
+            this.size = _size
+        }
+
+        public draw(): void {
+        }
+    }
+    function drawVertices() {
+        let lines = useful.createLines([[0,0,0],[1,1,1]],[cameraPos,cameraRotation])
+        for (let line of lines) {
+            OLED.drawLine(line[0][0], line[0][1], line[1][0], line[1][1], true)
+            console.log(line)
+        }
+    }
+    //% block="initalize OLED display"
+    //% block.loc.cs="inicializovat OLED display"
+    //% weight=100
+    export function init(): void {
+        OLED.init()
+        OLED.clear(false)
+    }
+    //% block="draw"
+    //% block.loc.cs="vykreslit"
+    //% weight=99
+    export function draw(): void {
+        for (let obj of objects) {
+            obj.draw()
+        }
+        OLED.draw()
+    }
+    //% block="cube|x $x|y $y|z $z|size $size"
+    //% block.loc.cs="krychle|x $x|y $y|z $z|velikost $size"
+    //% x.defl=0
+    //% y.delf=0
+    //% z.delf=0
+    //% size.delf=1
+    //% inlineInputMode=external
+    //% weight=98
+    export function addCube(x: number, y: number, z: number, size: number): Cube {
+        objects.push(new Cube(x, y, z, size))
+        return new Cube(x, y, z, size)
+    }
+}
+
+
 namespace useful {
     type Vertex = [number, number, number];
-    type Line = [[number,number], [number, number]];
+    type Line = [[number, number], [number, number]];
 
     type Camera = number[][];
 
@@ -32,10 +91,8 @@ namespace useful {
             vertex[2] - camera[0][2],
         ];
 
-        // Natočení souřadnic podle natočení kamery
         const rotatedX = rotateX(rotateY(rotateZ([x, y, z], camera[1][2]), camera[1][1]), camera[1][0]);
-        // Projekce na rovinu XZ (ignoruje třetí souřadnici)
-        const scaleFactor = 10; // Měřítko pro změnu velikosti výsledného obrazu
+        const scaleFactor = 10;
         const screenX = rotatedX[0] * scaleFactor + 64;
         const screenY = rotatedX[1] * scaleFactor + 32;
 
@@ -54,61 +111,5 @@ namespace useful {
         }
 
         return lines;
-    }
-}
-
-//% block="OLED 3D" icon="\uf1b2" color=#0000ff
-namespace OLED_3D {
-    let objects: any[] = []
-    let cameraPos = [0, 0, 0]
-    let cameraRotation = [0, 90, 0]
-    export class Cube {
-        public x: number
-        public y: number
-        public z: number
-        public size: number
-        constructor(_x: number, _y: number, _z: number, _size: number) {
-            this.x = _x
-            this.y = _y
-            this.z = _z
-            this.size = _size
-        }
-
-        public draw(): void {
-        }
-    }
-    function drawVertices() {
-        let lines = useful.createLines([[0,0,0],[1,1,1]],[cameraPos,cameraRotation])
-        for (let line of lines) {
-            OLED.drawLine(line[0][0], line[0][1], line[1][0], line[1][1], true)
-        }
-    }
-    //% block="initalize OLED display"
-    //% block.loc.cs="inicializovat OLED display"
-    //% weight=100
-    export function init(): void {
-        OLED.init()
-        OLED.clear(false)
-    }
-    //% block="draw"
-    //% block.loc.cs="vykreslit"
-    //% weight=99
-    export function draw(): void {
-        for (let obj of objects) {
-            obj.draw()
-        }
-        OLED.draw()
-    }
-    //% block="cube|x $x|y $y|z $z|size $size"
-    //% block.loc.cs="krychle|x $x|y $y|z $z|velikost $size"
-    //% x.defl=0
-    //% y.delf=0
-    //% z.delf=0
-    //% size.delf=1
-    //% inlineInputMode=external
-    //% weight=98
-    export function addCube(x: number, y: number, z: number, size: number): Cube {
-        objects.push(new Cube(x, y, z, size))
-        return new Cube(x, y, z, size)
     }
 }
