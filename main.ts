571a20b2a9a20356e669e3071809926f0a27761e
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-02-22 14:37:05 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -39,11 +39,10 @@ namespace OLED_3D {
 
         public draw(): void {
             let lines = projectLines([[0,0,0],[1,1,1]])
-            //onsole.log(lines.length)
-            for (let i = 0; i < lines.length; i+=2) {
+            for (let i = 0; i < lines.length; i += 2) {
+                console.log(i)
                 OLED.drawLine(lines[i][0], lines[i][1], lines[i+1][0], lines[i+1][1], true)
             }
-            console.log(lines)
         }
     }
     //% block="initalize OLED display"
