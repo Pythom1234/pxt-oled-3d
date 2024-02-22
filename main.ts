dafcd3a48f48d27225eaa9fe26361f8fdfdc6c84
Pythom1234 <pythom1234@email.cz>
2024-02-22 14:40:31 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -38,9 +38,9 @@ namespace OLED_3D {
         }
 
         public draw(): void {
-            let lines = projectLines([[0,0,0],[1,1,1]])
+            let lines = projectLines([[0, 0, 0], [1, 1, 1]])
+            console.log(lines[0])
             for (let i = 0; i < lines.length; i += 2) {
-                console.log(lines)
                 OLED.drawLine(lines[i][0], lines[i][1], lines[i + 1][0], lines[i + 1][1], true)
             }
         }
