702b48f59bfea46c1d180e273fb337b28c11947d
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-02-23 13:25:28 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ function projectLines(vertices: number[][]): number[][] {
     }
     return result
 }
-//% block="OLED 3D" icon="\uf1b2" color=#0000ff
+//% block="OLED 3D" icon="\uf1b2" color=#0050ff
 namespace OLED_3D {
     let objects: Cube[] = []
     let cameraPos = [0, 0, 0]
