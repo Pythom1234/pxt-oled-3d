6a0fb29453148531621366718193805aa51b04b8
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-03-03 15:35:08 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -93,6 +93,8 @@ namespace OLED_3D {
         public draw(): void {
             let lines = projectPoints(this.vertices,[0,0,0],[0,0,0])
             console.log(lines[0])
+            console.log(lines[1])
+            console.log(lines[2])
             for (let i = 0; i < lines.length; i += 2) {
                 OLED.drawLine(lines[i][0], lines[i][1], lines[i + 1][0], lines[i + 1][1], true)
             }
